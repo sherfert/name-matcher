d79b0d6e066f59f740c6e6750b42eab99d4cde30
Satia Herfert <satiaherfert@gmx.de>
2020-12-22 12:49:11 +0100

    Display icon in NameRater.

--- client/src/elements/NameRater.js
+++ client/src/elements/NameRater.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {faMars, faVenus, faVenusMars} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 class NameRater extends React.Component {
     constructor(props) {
@@ -7,9 +9,21 @@ class NameRater extends React.Component {
         };
     }
 
+    iconFor(sex) {
+        switch (sex) {
+            case "boy":
+                return <FontAwesomeIcon icon={faMars} />;
+            case "girl":
+                return <FontAwesomeIcon icon={faVenus} />;
+            case "neutral":
+            default:
+                return <FontAwesomeIcon icon={faVenusMars} />
+        }
+    }
+
     render() {
         return (
-            <div>{this.props.name} is a {this.props.sex}</div>
+            <div>{this.props.name} {this.iconFor(this.props.sex)}</div>
         );
     }
 }
